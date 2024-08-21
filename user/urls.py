d1ace69185929ce3b26d2235b8ce8511acfd0670
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-08-21 12:31:23 +0300

    ADD:   get new token   logout

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenBlacklistView, TokenRefreshView
+
 
 from .views import (AuthViewSet,
                     UpdateUserViewSet,
@@ -14,4 +16,6 @@ router.register(r'user/update-email', UpdateUserEmailViewSet, basename='update_e
 
 urlpatterns = router.urls + [
     path('user/', UpdateUserViewSet.as_view({'get': 'retrieve', 'put': 'update'})),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', TokenBlacklistView.as_view(), name='logout'),
 ]
