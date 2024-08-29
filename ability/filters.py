dc89b7dafa17058b062b5d943cee4247b0363a7e
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-08-29 14:45:04 +0300

    ADDED: filter for Wish

--- ability/filters.py
+++ ability/filters.py
@@ -3,7 +3,7 @@ from .models import Wish
 
 
 class WishFilter(django_filters.FilterSet):
-    is_fully_created = django_filters.BooleanFilter()
+    is_fully_created = django_filters.BooleanFilter(field_name='is_fully_created')
 
     class Meta:
         model = Wish
