57a27589045a2dc5678f06484bd1a9ca94cf2aa0
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-09-05 13:49:32 +0300

    UPDATE: image size in Wish

--- ability/choices.py
+++ ability/choices.py
@@ -14,4 +14,11 @@ access_type_choices = [
         ('subscribers', 'Subscribers'),
         ('everyone', 'Everyone'),
         ('only_me', 'Only_me'),
-    ]
+]
+
+image_size_choices = [
+        ("9:16", "9x16"),
+        ("3:4", "3x4"),
+        ("1:1", "1x1"),
+        ("4:3", "4x3"),
+]
