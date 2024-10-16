c99825f76e38de08127eda4f78461c6a83a3be5c
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-10-16 17:26:01 +0300

    UPDATE: added field about user

--- backend/settings.py
+++ backend/settings.py
@@ -196,16 +196,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-#AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-#AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-#AWS_S3_REGION_NAME = 'us-east-1'
-#AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-#DEFAULT_FILE_STORAGE = 'backend.storages.CustomS3Boto3Storage'
-#
-#
-#MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-MEDIA_URL = f'/media/'
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = 'us-east-1'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+DEFAULT_FILE_STORAGE = 'backend.storages.CustomS3Boto3Storage'
+
+
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 
 
